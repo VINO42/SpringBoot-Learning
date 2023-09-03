6e1cbe6ec952b725d8ae83df30230acfac879ad8
vino42 <38912428@qq.com>
2023-09-04 04:50:18 +0800

    optimize

--- springboot-p6spy/src/main/java/io/github/vino42/configuration/P6SpyLoggerStrategy.java
+++ springboot-p6spy/src/main/java/io/github/vino42/configuration/P6SpyLoggerStrategy.java
@@ -16,17 +16,23 @@ public class P6SpyLoggerStrategy implements MessageFormattingStrategy {
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         System.out.println();
         if (!"".equals(sql.trim())) {
-            String sqlBegin = "\n"+"============== SQL LOGGER BEGIN ==============";
-
-            String sqlExecuteTime = "SQL 执行时间       :" + DateUtil.format(new Date(),DatePattern.NORM_DATETIME_MS_PATTERN) + "\n";
+            StringBuffer sb = new StringBuffer();
+            sb.append("\r\n" + "============== SQL LOGGER BEGIN ==============");
+            sb.append("\r\n");
+            String sqlExecuteTime = "SQL 执行时间       :" + DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MS_PATTERN) + "\n";
+            sb.append(sqlExecuteTime);
             String elapsedStr = "SQL 执行毫秒       :" + elapsed + "ms" + "\n";
+            sb.append(elapsedStr);
             String sqlPrint = "SQL 执行语句       :" + sql;
-
+            sb.append(sqlPrint);
+            sb.append("\r\n");
             //String sqlPrint = !"".equals(sql.trim()) ? this.format.format(new Date()) + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + "\n " + sql + ";" : "";
-            String sqlEnd = "==============  SQL LOGGER END  =============="+"\n";
+            String sqlEnd = "==============  SQL LOGGER END  ==============" + "\n";
+            sb.append(sqlEnd);
 
-            return sqlBegin + "\r\n" + sqlExecuteTime + elapsedStr + sqlPrint + "\r\n" + sqlEnd;
+            return sb.toString();
         }
-        return ""; }
+        return "";
+    }
 }
 
