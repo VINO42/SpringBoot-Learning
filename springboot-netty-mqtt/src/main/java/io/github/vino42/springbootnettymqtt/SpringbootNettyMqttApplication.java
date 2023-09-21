7df698ce8b3f96f4d14854196b578e4e2d77e8d1
vino42 <38912428@qq.com>
2023-09-21 17:59:41 +0800

    springboot+netty+mqtt

---
+++ springboot-netty-mqtt/src/main/java/io/github/vino42/springbootnettymqtt/SpringbootNettyMqttApplication.java
@@ -0,0 +1,13 @@
+package io.github.vino42.springbootnettymqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNettyMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettyMqttApplication.class, args);
+    }
+
+}
